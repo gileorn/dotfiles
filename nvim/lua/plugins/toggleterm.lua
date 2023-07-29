6eda53b4cceb01c0abe5927f1aad2bfa6e7874ee
Sergei Iakovlev <sergei.iakovlev@manychat.com>
2023-07-29 09:41:41 +0200

    make lazygit fullscreen with toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,13 @@
+require("toggleterm").setup({
+	float_opts = { width = 1000, height = 1000 },
+})
+
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
 function _lazygit_toggle()
 	lazygit:toggle()
 end
-
-require("toggleterm").setup()
 
 vim.api.nvim_set_keymap(
 	"n",
