2d66b23a10e8007ee67c1b7c62f795c5303564dd
Sergei Iakovlev <sergei.iakovlev@manychat.com>
2023-08-02 15:34:12 +0200

    add delete and change keymaps to surround plugin

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,15 +1,23 @@
 require("nvim-surround").setup({
+	-- changed keymaps so that leap and surround plugins could happily live together
 	-- see https://github.com/ggandor/leap.nvim/discussions/59
 	keymaps = {
+		normal = "gz",
+		delete = "gzd",
+		change = "gzr",
 		-- insert = "<C-g>z",
 		-- insert_line = "gC-ggZ",
-		normal = "gz",
 		-- normal_cur = "gZ",
 		-- normal_line = "gzgz",
 		-- normal_cur_line = "gZgZ",
 		-- visual = "gz",
 		-- visual_line = "gZ",
-		-- delete = "gzd",
-		-- change = "gzr",
 	},
 })
+
+-- to wrap a string with a set of parens we would use gziw(
+-- - `gz` to trigger the surround plugin
+-- - `iw` is vim motion to sellect inner word
+-- - ( indicates the character to surround the word
+-- to delete the surrounding pair of parens we would use `gzd(`
+-- to change the surrounding pair of parens to brackets we would use `gzr([`
