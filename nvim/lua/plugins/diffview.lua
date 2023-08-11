5f6d1ede8f7bc54c56ea0b4d6ed91dee9bf1d909
Sergei Iakovlev <sergei.iakovlev@manychat.com>
2023-08-11 12:51:25 +0200

    change default diffview layout to vertical

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,21 +1,26 @@
 local actions = require("diffview.actions")
 
 require("diffview").setup({
-	keymaps = {
-		view = {
-			["o"] = actions.goto_file_edit,
-		},
-		file_panel = {
-			["o"] = actions.goto_file_edit,
-			["j"] = actions.select_next_entry,
-			["k"] = actions.select_prev_entry,
-			["?"] = "<Cmd>h diffview-maps-file-panel<CR>",
-		},
-		file_history_panel = {
-			["o"] = actions.goto_file_edit,
-			["j"] = actions.select_next_entry,
-			["k"] = actions.select_prev_entry,
-			["?"] = "<Cmd>h diffview-maps-file-history-panel<CR>",
-		},
-	},
+  keymaps = {
+    view = {
+      ["o"] = actions.goto_file_edit,
+    },
+    file_panel = {
+      ["o"] = actions.goto_file_edit,
+      ["j"] = actions.select_next_entry,
+      ["k"] = actions.select_prev_entry,
+      ["?"] = "<Cmd>h diffview-maps-file-panel<CR>",
+    },
+    file_history_panel = {
+      ["o"] = actions.goto_file_edit,
+      ["j"] = actions.select_next_entry,
+      ["k"] = actions.select_prev_entry,
+      ["?"] = "<Cmd>h diffview-maps-file-history-panel<CR>",
+    },
+  },
+  view = {
+    default = {
+      layout = "diff2_vertical",
+    },
+  },
 })
