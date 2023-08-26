b90dd4648c01c296b45e31172f94d64dcd39e80c
Sergei Iakovlev <sergei.iakovlev@manychat.com>
2023-08-26 11:45:02 +0200

    adjust width in diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,26 +1,37 @@
 local actions = require("diffview.actions")
 
 require("diffview").setup({
-	keymaps = {
-		view = {
-			["o"] = actions.goto_file_edit,
-		},
-		file_panel = {
-			["o"] = actions.goto_file_edit,
-			["k"] = actions.select_next_entry,
-			["l"] = actions.select_prev_entry,
-			["?"] = "<Cmd>h diffview-maps-file-panel<CR>",
-		},
-		file_history_panel = {
-			["o"] = actions.goto_file_edit,
-			["k"] = actions.select_next_entry,
-			["l"] = actions.select_prev_entry,
-			["?"] = "<Cmd>h diffview-maps-file-history-panel<CR>",
-		},
-	},
-	view = {
-		default = {
-			layout = "diff2_vertical",
-		},
-	},
+  keymaps = {
+    view = {
+      ["o"] = actions.goto_file_edit,
+    },
+    file_panel = {
+      ["o"] = actions.goto_file_edit,
+      ["k"] = actions.select_next_entry,
+      ["l"] = actions.select_prev_entry,
+      ["?"] = "<Cmd>h diffview-maps-file-panel<CR>",
+    },
+    file_history_panel = {
+      ["o"] = actions.goto_file_edit,
+      ["k"] = actions.select_next_entry,
+      ["l"] = actions.select_prev_entry,
+      ["?"] = "<Cmd>h diffview-maps-file-history-panel<CR>",
+    },
+  },
+  view = {
+    default = {
+      layout = "diff2_vertical",
+    },
+  },
+  file_panel = {
+    listing_style = "tree",
+    tree_options = {
+      flatten_dirs = true,
+      folder_statuses = "only_folded",
+    },
+    win_config = {
+      position = "left",
+      width = 70,
+    },
+  },
 })
