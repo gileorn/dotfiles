0ddd1605b883047cb09eac0b3bd439e3c19ef4ca
Sergei Iakovlev <sergei.iakovlev@manychat.com>
2024-03-26 18:20:00 +0100

    move from eslint format on save to format on commit

---
+++ .zshrc
@@ -0,0 +1,13 @@
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+zstyle ':vcs_info:git:*' formats '%b '
+
+setopt PROMPT_SUBST
+PROMPT='%F{green}%*%f %F{blue}%~%f %F{red}${vcs_info_msg_0_}%f$ '
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+alias mlint="npx eslint --fix"
--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -6,11 +6,11 @@ return {
 
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier", "eslint_d" },
-				typescript = { "prettier", "eslint_d" },
-				javascriptreact = { "prettier", "eslint_d" },
-				typescriptreact = { "prettier", "eslint_d" },
-				svelte = { "prettier", "eslint_d" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
 				css = { "prettier", "stylelint" },
 				html = { "prettier" },
 				json = { "prettier" },
